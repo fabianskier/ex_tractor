566ff14fd613c8ee0ed43caebce9e03753c2dba9
fabianskier <fabianskier@icloud.com>
2023-06-27 23:36:29 -0400

    Initial commit

---
+++ lib/ex_tractor.ex
@@ -0,0 +1,18 @@
+defmodule ExTractor do
+  @moduledoc """
+  Documentation for `ExTractor`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTractor.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_tractor_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTractorTest do
+  use ExUnit.Case
+  doctest ExTractor
+
+  test "greets the world" do
+    assert ExTractor.hello() == :world
+  end
+end
